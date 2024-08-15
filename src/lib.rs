7455b935eac2cf43714c4289d969c251d9fc9cd9
vishnuMohonan01 <iamvm.dev@gmail.com>
2024-08-15 16:12:07 +0530

    Add case insensitive search + print error to stderr

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,19 @@
 use std::fs;
 use std::error::Error;
+use std::env;
 
 pub struct Config {
   pub query: String,
-  pub filename: String
+  pub filename: String,
+  pub case_insensitive: bool
+}
+
+fn string_to_bool(str: &str) -> bool {
+  match str {
+    "false" => false,
+    "true" => true,
+    _ => true
+  }
 }
 
 impl Config {
@@ -15,14 +25,23 @@ impl Config {
       let query = args[1].clone();
       let filename = args[2].clone();
 
-      Ok(Config { query, filename })
+      let case_insensitive = env::var("CASE_INSENSITIVE").unwrap_or_else(|_| {"false".to_string()});
+      let case_insensitive = string_to_bool(&case_insensitive);
+
+      Ok(Config { query, filename, case_insensitive })
   }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
   let contents = fs::read_to_string(config.filename)?;
 
-  for line in search(&contents, &config.query) {
+  let results: Vec<&str> = if config.case_insensitive {
+    insensitive_search(&contents, &config.query)
+  } else {
+    search(&contents, &config.query)
+  };
+
+  for line in results {
     println!("{}", line);
   }
 
@@ -34,6 +53,18 @@ fn search<'a>(content: &'a str, query: &'a str) -> Vec<&'a str> {
 
   for line in content.lines() {
     if line.contains(query) {
+      results.push(line);
+    }
+  }
+
+  results
+}
+
+fn insensitive_search<'a>(content: &'a str, query: &'a str) -> Vec<&'a str> {
+  let mut results= Vec::new();
+
+  for line in content.lines() {
+    if line.to_lowercase().contains(&query.to_lowercase()) {
       results.push(line);
     }
   }
@@ -55,5 +86,17 @@ Pick three.
 ";
 
     assert_eq!(vec!["Safe, fast, productive."], search(content, query));
+  }
+
+  #[test]
+  fn insensitive_search_returns_case_insensitive_results() {
+    let query = "Duct";
+    let content = "\
+Rust:
+Safe, fast, productive.
+Pick three.
+";
+
+    assert_eq!(vec!["Safe, fast, productive."], insensitive_search(content, query));
   }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Error: {}", err);
+        eprintln!("Error: {}", err);
         process::exit(1);
     });
 
-    println!("Searching for: {:?}", config.query);
-    println!("In file: {:?}", config.filename);
-
     if let Err(e) = minigrep::run(config) {
-        println!("Error: {}", e);
+        eprintln!("Error: {}", e);
         process::exit(1);
     }
 }
